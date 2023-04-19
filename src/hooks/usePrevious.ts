1a0b74549c0240ad434edebfa12448e82544ef00
tsunodajapa <gabrieltsunoda@gmail.com>
2023-04-18 22:08:01 -0300

    fix: add type in usePrevious

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,9 +1,11 @@
 import { useEffect, useRef } from "react";
 
-export function usePrevious(value: any) {
-  const ref = useRef();
+export function usePrevious<T>(value: T): T | null {
+  const ref = useRef<T | null>(null);
+
   useEffect(() => {
     ref.current = value;
   }, [value]);
+
   return ref.current;
 }
